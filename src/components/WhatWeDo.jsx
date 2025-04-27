0781c66baee95cc63db6c491cb4d5bc7fc7ef63e
Apoorva Singh <apoorvasingh547@gmail.com>
2025-04-27 22:44:35 +0530

    Fix

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -49,12 +49,19 @@ const WhatWeDo = () => {
     arrows: false,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: { slidesToShow: 2 }
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
       },
       {
-        breakpoint: 600,
-        settings: { slidesToShow: 1 }
+        breakpoint: 400,
+        settings: { 
+          slidesToShow: 1 ,
+          slidesToScroll: 1
+        }
       }
     ]
   };
